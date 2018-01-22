b06756de6f7c0053f3cd95234847d63fc08e10e0
sunhonglong <412462819@qq.com>
2018-01-22 15:57:35 +0800

    1

--- src/main/java/com/upyoo/cloud/common/api/CooperationAppApi.java
+++ src/main/java/com/upyoo/cloud/common/api/CooperationAppApi.java
@@ -1,6 +1,5 @@
 package com.upyoo.cloud.common.api;
 
-import java.util.Map;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.upyoo.cloud.common.entity.CooperationApp;
 import com.upyoo.cloud.common.entity.ResultModel;
 
 @FeignClient("automation")
 @RequestMapping("/autoapp")
 public interface CooperationAppApi {
+	
 	@GetMapping(value="/{owner}",produces ="application/json; charset=utf-8")
 	public ResultModel queryAllCooperationAppByOwner(@PathVariable("owner") String owner) ;
 	
@@ -23,7 +24,7 @@ public interface CooperationAppApi {
 	public ResultModel get(@RequestParam("id") String id) ;
 	
 	@PostMapping(value="/{owner}")
-	public ResultModel create(@PathVariable("owner")  String owner,@RequestBody Map<String, String> autoApp);
+	public ResultModel create(@PathVariable("owner")  String owner,@RequestBody CooperationApp cooperationApp);
 	
 	@DeleteMapping(value="/{id}")
 	public ResultModel  delete(@PathVariable("id") String id) ;
