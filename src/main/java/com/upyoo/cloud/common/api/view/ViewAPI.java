9560b4a723e93e3a2515e725b03730e699fb126f
wangxu <wangxu@oneapm.com>
2018-04-13 12:34:42 +0800

    add new interface

--- src/main/java/com/upyoo/cloud/common/api/view/ViewAPI.java
+++ src/main/java/com/upyoo/cloud/common/api/view/ViewAPI.java
@@ -8,13 +8,21 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("view")
 public interface ViewAPI {
 
     @PostMapping("/form")
     public ResultModel saveForm(@RequestBody UiForm form);
+
+    @PutMapping("/form/{id}")
+    public ResultModel updateNameById(@PathVariable("id") String id, @RequestParam("name") String name);
+
+    @PutMapping("/form/{objectName}/{id}")
+    public ResultModel updateIsDefault(@PathVariable("objectName") String objectName, @PathVariable("id") String id);
 
     @GetMapping("/form/{id}")
     public ResultModel queryFormById(@PathVariable("id") String id);
